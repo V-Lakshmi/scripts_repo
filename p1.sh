46eb7e0612277f4c59ae06ceccbc8fd0e69be665
lakshmiv <shreelakshmi6v@gmail.com>
2022-03-14 18:30:14 +0000

    scripts class1

---
+++ p1.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "script name is $0"
+
+echo "name is $1"
+echo "from $2"
+echo "age is $3"
+
---
+++ p2.sh
@@ -0,0 +1,21 @@
+
+
+
+
+
+
+
+
+#!/bin/bash
+
+
+
+
+
+
+
+var1="stu"
+var2="def"
+var3="28"
+
+echo "name is $var1, from $var2 and age is $var3"
